e271879db4a654e13e5ad52cc11120be0286645b
Alec Missine <alec_missine@yahoo.com>
2021-06-05 16:18:58 -0400

    Add config to the workers

--- binance.js
+++ binance.js
@@ -3,20 +3,50 @@
 //
 (function () {
 
-fetch('https://api.binance.com/api/v3/exchangeInfo')
-  .then(response => response.json())
-  .then(data => postMessage(pairs(data)))
-
-onmessage = e => {
-  console.log(e.data)
-}
-
-function pairs (data) {
-  let result = []
-  for (const item of data.symbols) {
-    result.push({ pair: item.baseAsset + '-' + item.quoteAsset })
+  const config = {
+    url: 'https://api.binance.com/api/v3',
+    urlPairs: u => `${u}/exchangeInfo`,
+    urlBook: (u, s) => `${u}/depth?symbol=${s}&limit=5`,
+    rateLimitMs: 2500,
   }
-  return result;
-}
+
+  doGet(config.urlPairs(config.url), data => postMessage(pairs(data)))
+
+  onmessage = e => {
+    let symbol = e.data.reduce((a, c) => a + c),
+      count = 0, requestInProgress = false, lastUpdateIdP = -1,
+      feed = setInterval(() => {
+        if (requestInProgress) {
+          console.log('- REQUEST IN PROGRESS')
+        } else {
+          doGet(config.urlBook(config.url, symbol), orderBook)
+          if (++count > 9) {
+            clearInterval(feed); postMessage(null)
+          }
+          requestInProgress = true
+        }
+      }, config.rateLimitMs)
+
+    function orderBook (data) {
+      requestInProgress = false
+      if (data.lastUpdateId == lastUpdateIdP) {
+        return;
+      }
+      lastUpdateIdP = data.lastUpdateId
+      postMessage(data)
+    }
+  }
+
+  function doGet (url, cb) {
+    fetch(url).then(response => response.json()).then(cb)
+  }
+
+  function pairs (data) {
+    let result = []
+    for (const item of data.symbols) {
+      result.push({ pair: item.baseAsset + '-' + item.quoteAsset })
+    }
+    return result;
+  }
 
 })()
--- coinbase.js
+++ coinbase.js
@@ -3,20 +3,51 @@
 //
 (function () {
 
-fetch('https://api.pro.coinbase.com/products')
-  .then(response => response.json())
-  .then(data => postMessage(pairs(data)))
-
-onmessage = e => {
-  console.log(e.data)
-}
-
-function pairs (data) {
-  let result = []
-  for (const item of data) {
-    result.push({ pair: item.id })
+  const config = {
+    url: 'https://api.pro.coinbase.com/products',
+    urlBook: (u, p) => `${u}/${p}/book?level=2`,
+    rateLimitMs: 2500,
   }
-  return result;
-}
+
+  doGet(config.url, data => postMessage(pairs(data)))
+
+  onmessage = e => {
+    let product = e.data[0] + '-' + e.data[1],
+      count = 0, requestInProgress = false, sequenceP = -1,
+      feed = setInterval(() => {
+        if (requestInProgress) {
+          console.log('- REQUEST IN PROGRESS')
+        } else {
+          doGet(config.urlBook(config.url, product), orderBook)
+          if (++count > 9) {
+            clearInterval(feed); postMessage(null)
+          }
+          requestInProgress = true
+        }
+      }, config.rateLimitMs)
+
+    function orderBook (data) {
+      requestInProgress = false
+      if (data.sequence == sequenceP) {
+        return;
+      }
+      sequenceP = data.sequence
+      postMessage(data)
+    }
+  }
+
+  function doGet (url, cb) {
+    fetch(url).
+      then(response => response.json()).
+      then(cb)
+  }
+
+  function pairs (data) {
+    let result = []
+    for (const item of data) {
+      result.push({ pair: item.id })
+    }
+    return result;
+  }
 
 })()
