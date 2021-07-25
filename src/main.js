b6076fb2c2b93445c371e4dfc39f2008866fff08
Alec Missine <alec_missine@yahoo.com>
2021-07-25 08:47:39 -0600

    Replace google with localhost

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { config } from './config.js'
 import feed from './feed.js'
 
+let header = document.getElementById('header')
+header.textContent = 'Arbitrage opportunities since ' + new Date()
 config('arbitrage')(feed)
