87308e5e9dab016ed79a8da18732e7fccdcc8017
Alec Missine <alec_missine@yahoo.com>
2021-07-25 16:23:20 -0600

    Use postMessage with commands

--- src/main.js
+++ src/main.js
@@ -3,4 +3,9 @@ import feed from './feed.js'
 
 let header = document.getElementById('header')
 header.textContent = 'Arbitrage opportunities since ' + new Date()
+
+const onmessage = e => {
+  console.log(e.data)
+}
+window.addEventListener("message", onmessage)
 config('arbitrage')(feed)
